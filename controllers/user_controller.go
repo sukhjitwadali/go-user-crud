78b553e6a84bc91a29aa454acb98030e6b51b77c
sukhwadali1 <sukhwadali1@gmail.com>
2024-10-05 14:55:00 -0400

    create user cont

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"encoding/json"
 	"go-user-crud/models"
+	"net/http"
 	"sync"
 )
 
@@ -10,3 +12,20 @@ var (
 	nextID   = 1
 	usersMux sync.Mutex
 )
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err == nil {
+		http.Error(w, "invalid input", http.StatusBadRequest)
+		return
+	}
+	usersMux.Lock()
+	user.Id = nextID
+	users[nextID] = user
+	nextID++
+	usersMux.Unlock()
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
+}
